81235173867644c01ea15df80f3939358d0aac75
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-02-06 15:53:40 -0300

    Iniciando no useContext

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
-import P from 'prop-types';
 
-const Button = ({ incrementButton }) => {
-  return <button onClick={() => incrementButton(10)}>+</button>;
+const globalState = {
+  title: 'O titulio desse contexto',
+  body: 'body do context',
+  counter: 0,
 };
 
-Button.propTypes = {
-  incrementButton: P.func,
+const GlobalContext = React.createContext();
+
+// eslint-disable-next-line
+const Div = ({ children }) => {
+  return (
+    <>
+      <H1 />
+      <P />
+    </>
+  );
+};
+
+// eslint-disable-next-line
+const H1 = ({ children }) => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { title, counter },
+  } = theContext;
+  return (
+    <h1>
+      {title} {counter}
+    </h1>
+  );
+};
+
+// eslint-disable-next-line
+const P = ({ children }) => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { body },
+    setContextState,
+  } = theContext;
+  return <p onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}>{body}</p>;
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  // useCallBack
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
-  }, []);
-
-  const btn = useMemo(() => {
-    return <Button incrementButton={incrementCounter}>cont 1 = +</Button>;
-  }, [incrementCounter]);
+  const [contextState, setContextState] = useState(globalState);
 
   return (
-    <div className="App">
-      <h1>Contador 1: {counter}</h1>
-      {btn}
-    </div>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
   );
 }
 
