45eef476df867a79995cab71348131eb16b47168
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-02-06 12:26:39 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  // componentDidUpdate - executa toda o vez que o componente atualiza
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+  // componentDidMount - executa uma vez
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, []);
+
+  // componentWillMount - executa quando o componente for desmontado
+  useEffect(() => {
+    return () => {
+      console.log('componentWillMount');
+    };
+  }, []);
+
+  // Com dependencia - executa toda que a dependencia mudar
+  useEffect(() => {
+    console.log('counter mudou', counter);
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contador 1: {counter}</h1>
+      <h1>Contador 2: {counter2}</h1>
+      <button onClick={() => setCounter(counter + 1)}>cont 1 = +</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>cont 2 = +</button>
     </div>
   );
 }
